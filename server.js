3579a0371c7afc0744444ed104a49f0584768c70
Gil Tayar <gilt@wix.com>
2014-08-06 17:17:11 +0300

    version

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ http.createServer(function(req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
 
   fs.readFile('D:\\home\\site\\repository\\.git\\config', function(err, file) {
-      res.end('vavava Read file: ' + file.toString() + "; " + err);
+      res.end('vavava version:' + process.version);
   })
 }).listen(port);
